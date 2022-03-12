4c5c58ed7e10eab971faaaddf7c82e4e9c0b55b5
Sinhalite <16186206+Sinhalite@users.noreply.github.com>
2022-03-12 20:47:41 +0900

    Add link

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Link href={'/test'} >Test Page</Link>
+      <Link href={'/333'} >Test Page(no paths)</Link>
       <br />
       <a href='/api/revalidate' target='_blank' rel="noopener noreferrer">
         Revalidate
