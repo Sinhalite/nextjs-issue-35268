e1eedf2dcdba677c4300a431f8eee2303ed510d8
Sinhalite <16186206+Sinhalite@users.noreply.github.com>
2022-03-12 14:32:57 +0900

    First commit

---
+++ pages/[id].js
@@ -0,0 +1,32 @@
+export default function TestPage({ currentTime }) {
+  return (
+    <div>
+      { currentTime }
+    </div>
+  )
+}
+
+export function getStaticPaths() {
+  return {
+    paths: ['/test'],
+    fallback: 'blocking'
+  }
+}
+
+export async function getStaticProps() {
+
+  function sleep(waitMsec) {
+    const startMsec = new Date();
+    while (new Date() - startMsec < waitMsec);
+  }
+
+  sleep(8000)
+
+  const currentTime = Date.now()
+
+  return {
+    props: {
+      currentTime
+    },
+  }
+}
