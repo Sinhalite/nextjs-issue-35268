940e5e8073dec39744f44297be658a4aa1e7ec37
Sinhalite <16186206+Sinhalite@users.noreply.github.com>
2022-07-14 22:41:48 +0900

    test

--- pages/[id].js
+++ pages/[id].js
@@ -1,32 +1,26 @@
 export default function TestPage({ currentTime }) {
-  return (
-    <div>
-      { currentTime }
-    </div>
-  )
+  return <div>{currentTime}</div>;
 }
 
 export function getStaticPaths() {
   return {
-    paths: ['/test'],
-    fallback: 'blocking'
-  }
+    paths: [
+      {
+        params: {
+          id: "test",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
 }
 
 export async function getStaticProps() {
-
-  function sleep(waitMsec) {
-    const startMsec = new Date();
-    while (new Date() - startMsec < waitMsec);
-  }
-
-  sleep(8000)
-
-  const currentTime = Date.now()
+  const currentTime = Date.now();
 
   return {
     props: {
-      currentTime
+      currentTime,
     },
-  }
+  };
 }
