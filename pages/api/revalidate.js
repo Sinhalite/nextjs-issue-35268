f51fd54162ab70b220ac052bbed885180f1ede3d
Sinhalite <16186206+Sinhalite@users.noreply.github.com>
2022-07-16 07:06:33 +0900

    check

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
     await res.revalidate("/test");
     return res.json({ revalidated: true });
   } catch (err) {
-    console.log(err);
     return res.status(500).send("Error revalidating");
   }
 }
